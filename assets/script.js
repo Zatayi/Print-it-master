382d41e9a817d5efb76e3f9433565a762a354ff0
Boris Balligand <advent_children@hotmail.fr>
2023-12-26 19:06:48 +0100

    add polish js

--- assets/script.js
+++ assets/script.js
@@ -20,50 +20,52 @@ const slides = [
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 const bannerImg = document.querySelector('.banner-img')
+const dotContainer = document.querySelector('.dots')
+const tagLine = document.querySelector('#banner p')
 let position = 0;
+
+slides.forEach((slide, index) => {
+	const dot = document.createElement('div')
+	dot.setAttribute('class', index === 0 ? 'dot dot_selected' : 'dot')
+
+	dotContainer.appendChild(dot)
+})
 const dots = document.querySelectorAll('.dot');
 
 arrowLeft.addEventListener('click', function () {
 	position--;
-	imgcaroussel(position, 'left');
-	numberdots(position)
-	
+	updateCarrousel('left');
 })
 arrowRight.addEventListener('click', function () {
 	position++;
-	imgcaroussel(position, 'right');
-	numberdots(position)
-	
+	updateCarrousel('right');
 })
 console.log(arrowRight);
 console.log(arrowLeft);
 
 
-function imgcaroussel(index, sens) {
-	
+function updateCarrousel(sens) {
 	if (position === -1 && sens === 'left') {
-	  position = slides.length - 1;
-  } else if (position === slides.length && sens === 'right') {
-	  position = 0;
-  }
-  const imgderoule = `assets/images/slideshow/${slides[position].image}`;
-  bannerImg.src = imgderoule;
-  bannerImg.alt = `Slide ${position + 1}`;
-  
-  const tagLine = slides[position].tagLine;
-  document.querySelector('p').innerHTML = tagLine;
+		position = slides.length - 1;
+	} else if (position === slides.length && sens === 'right') {
+		position = 0;
+	}
+	
+	bannerImg.src = `assets/images/slideshow/${slides[position].image}`
+	bannerImg.alt = `Slide ${position + 1}`;
+	tagLine.innerHTML = slides[position].tagLine;
+
+	updateDotSelected()
 }
 
-function numberdots(index) {
-    dots.forEach((dot, i) => {
-        if (i === index) {
-            dot.classList.add('dot_selected'); 
-        } else {
-            dot.classList.remove('dot_selected'); 
-        }
-    });
+function updateDotSelected() {
+	dots.forEach((dot, i) => {
+		if (i === position) {
+			dot.classList.add('dot_selected');
+		} else {
+			dot.classList.remove('dot_selected');
+		}
+	});
 }
- 
-  
-  
+
 
